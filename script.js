29771e486563319c562b8c854691b58e25c33d68
Kyla Bendt <kylabendt@gmail.com>
2019-11-07 16:40:07 -0700

    make font change on mouseover

--- script.js
+++ script.js
@@ -1,3 +1,40 @@
 window.onload = function () {
+	// initPage();
+}
 
+
+function onMouseEnterP1() {
+	let paragraph = document.getElementById("p1");
+	paragraph.style.color = 'blue';
+}
+
+function onMouseLeaveP1() {
+	let paragraph = document.getElementById("p1");
+	paragraph.style.color = 'black';
+}
+
+function onMouseEnterP2() {
+	let paragraph = document.getElementById("p2");
+	paragraph.style.color = 'green';
+}
+
+function onMouseLeaveP2() {
+	let paragraph = document.getElementById("p2");
+	paragraph.style.color = 'black';
+}
+
+function onMouseEnterP3() {
+	let paragraph = document.getElementById("p3");
+	paragraph.style.color = 'red';
+}
+
+function onMouseLeaveP3() {
+	let paragraph = document.getElementById("p3");
+	paragraph.style.color = 'black';
+}
+
+function selectH1() {
+	let header = document.getElementById("h1");
+	console.log("Here I am");
+	header.style.fontFamily = 'cursive';
 }
