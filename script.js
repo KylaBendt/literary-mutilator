44d93bb4822bcf515b23b196daa231eefe79ddfe
Kyla Bendt <kylabendt@gmail.com>
2019-11-11 09:55:41 -0700

    make rot13 not work

--- script.js
+++ script.js
@@ -40,11 +40,14 @@ function selectH1() {
 }
 
 function onClickP1() {
-	let paragraph = document.getElementById("p1");
-	let cipher = {'A':'N', }
-	textArray = paragraph.textContent.split(' ');
-	for (let i =0; i < textArray.length; ++i) {
-		textArray[i]
+	console.log("onclickp1");
+	let p1 = document.getElementById("p1");
+	let cipher = ['A':'N', 'B':'O', 'C':'P', 'D':'Q', 'E':'R', 'F':'S', 'G':'T', 'H':'U', 'I':'V', 'J':'W', 'K':'X', 'L':'Y', 'M':'Z', 'N':'A', 'O':'B', 'P':'C', 'Q':'D', 'R':'E', 'S':'F', 'T':'G', 'U':'H', 'V':'I', 'W':'J', 'X':'K', 'Y':'L', 'Z':'M', 'a':'n', 'b':'o', 'c':'p', 'd':'q', 'e':'r', 'f':'s', 'g':'t', 'h':'u', 'i':'v', 'j':'w', 'k':'x', 'l':'y', 'm':'z', 'n':'a', 'o':'b', 'p':'c', 'q':'d', 'r':'e', 's':'f', 't':'g', 'u':'h', 'v':'i', 'w':'j', 'x':'k', 'y':'l', 'z':'m'];
+	//let textArray = paragraph.textContent.split('');
+	let paragraph = p1.textContent;
+	for (let i =0; i < paragraph.length; ++i) {
+		let a = paragraph[i];
+		paragraph[i] = cipher[a];
 	}
-
+	p1.textContent = paragraph;
 }
